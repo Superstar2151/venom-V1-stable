32ee607a15b7b862b4ca5d07a0ba26d9a297699c
Superstar2151 <superstar2151@gmail.com>
2024-08-29 21:14:53 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY VENOMxCRAZY
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("TOKEN")
+BOT_TOKEN=("7437728882:AAFDS-DVcm4QmjAKe_qKYA6UVgdvy4qoN3c")
 
   #Enter your group id here Get it by creating a group that group add your bot and ross bot then public it and type /id to get your id
-GROUP_ID=("GROUPid")
+GROUP_ID=("1002240839591")
 
 
 
